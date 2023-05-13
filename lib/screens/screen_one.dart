e02694e41eb4e3213928b7aae14b41be8637ba2f
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2023-05-13 22:03:09 +0100

    andriod unit initialzation

---
+++ lib/screens/screen_one.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ScreenOne extends StatefulWidget {
+  const ScreenOne({super.key});
+
+  @override
+  State<ScreenOne> createState() => _ScreenOneState();
+}
+
+class _ScreenOneState extends State<ScreenOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        //TODO:- Show banner here
+        child: Container(),
+      ),
+    );
+  }
+}
